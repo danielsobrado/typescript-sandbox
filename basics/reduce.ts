462b92fc6ff7baaedb90f65816082197ecac6b3a
danielsobrado <daniel@danielsobrado.com>
2022-01-19 08:10:15 +0400

    filter ids with reduce

--- basics/reduce.ts
+++ basics/reduce.ts
@@ -5,3 +5,16 @@ let arr2 = ['uno','dos','tres']
 let textAcum = arr2.reduce((previous, current) => { return previous + current + ' '},'');
 
 console.log(textAcum)
+
+interface User {
+    id: number,
+    name: string
+}
+
+const myUsers: User[] = [{id: 1, name: "Pedro"}, {id: 2, name: "Luis"}, {id: 3, name: "Dani"}]
+
+let filterIds = myUsers.reduce(
+        (previousId, currUser) => currUser.id % 2 === 1 ? [...previousId, currUser.id] : previousId , [] as number[]            
+    );
+
+console.log(filterIds)
